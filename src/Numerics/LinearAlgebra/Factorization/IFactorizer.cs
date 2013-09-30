ee5d89266046b2df6d9ea461e835d4fd9cdf6387
Travis Silvers <firestrand@gmail.com>
2013-09-29 23:12:48 -0400

    Start of NMF implementation

---
+++ src/Numerics/LinearAlgebra/Factorization/IFactorizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Numerics.LinearAlgebra.Factorization
+{
+    /// <summary>
+    /// Classes that factor a matrix into two components where the resulting matrixes W and H
+    ///  when multiplied together closely approximate the original matrix V, <c>V ≈ WH</c>.
+    /// The dimensions of the matrix factors W and H are n x r and r x m, respectively. 
+    /// The rank r of the factorization is generally chosen so that (n + m)r &lt; nm, and the product WH can be regarded as a compressed form of the data in V.
+    /// </summary>
+    /// <typeparam name="T">Supported data types are double, single, <see cref="Complex"/>, and <see cref="Complex32"/>.</typeparam>
+    public interface IFactorizer<T> where T : struct, IEquatable<T>, IFormattable
+    {
+        void Factor(Matrix<T> vInput, int rank, Matrix<T> wResult, Matrix<T> hResult);
+    }
+}
---
+++ src/Numerics/LinearAlgebra/Factorization/NMF.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Numerics.LinearAlgebra.Factorization
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class NMF<T> : IFactorizer<T>
+        where T : struct, IEquatable<T>, IFormattable
+    {
+        public void Factor(Matrix<T> vInput, int rank, Matrix<T> wResult, Matrix<T> hResult)
+        {
+            throw new NotImplementedException();
+        }
+        //TODO: Cost functions:
+        ///http://hebb.mit.edu/people/seung/papers/nmfconverge.pdf
+        //1. Euclidean Distance
+        //2. Divergence
+    }
+}
